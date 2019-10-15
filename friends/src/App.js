7ac1653c562ac5d00a54d6f8062642512cc8fd4d
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-15 14:24:12 +0100

    create axios with auth using token gotten from the user password

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './App.css';
 import { Route, NavLink } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
-
+import FriendList from './components/FriendList';
 
 function App() {
   return (
     <div className="App">
       <nav className="nav">
         <NavLink exact to='/'>Form</NavLink>
+        <NavLink to='/friendList'>Friends</NavLink>
       </nav>
 
       <main>
         <Route exact path='/' component={LoginForm} />
+        <Route path='/friendList' component={FriendList} />
       </main>
     </div>
   );
--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import axios from 'axios';
 
-export default function () {
+export default function FriendList () {
+    const [ friends, setFriends ] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/friends')
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
     return(
         <div>
-            
+
         </div>
     );
 }
