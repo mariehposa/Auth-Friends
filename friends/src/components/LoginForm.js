4d8745598e2dd578dfd194246a19e95b5e99e92c
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-15 13:57:20 +0100

    login function save the returned token to localStorage

--- friends/src/components/LoginForm.js
+++ friends/src/components/LoginForm.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import axios from 'axios';
 
 export default function LoginForm () {
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+
+    const submit = () => {
+        axios.post('http://localhost:5000/api/login', {
+            username: usernameRef.current.value,
+            password: passwordRef.current.value,
+        })
+            .then(res => {
+                //console.log(res);
+                localStorage.setItem('token', res.data.payload)
+            })
+            .catch(err => {
+                //console.log(err.response.data.error)
+                alert(err.response.data.error)
+            })
+    }
+
     return (
         <div>
-            username <input type="text" placeholder="Enter username"/>
+            username <input type="text" placeholder="Enter username" ref={usernameRef}/>
             <br />
-            password <input type="text" placeholder="Enter password" />
+            password <input type="text" placeholder="Enter password" ref={passwordRef} />
             <br />
-            <button>Submit</button>
+            <button onClick = {submit}>Submit</button>
         </div>
     );
 }
