c79b81520c83ccb3bc14d6a04ce097d7b4e8ba03
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-15 12:10:09 +0100

    create a simple form for the user

---
+++ friends/src/components/LoginForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function LoginForm () {
+    return (
+        <div>
+            username <input type="text" placeholder="Enter username"/>
+            password <input type="text" placeholder="Enter password" />
+            <button>Submit</button>
+        </div>
+    );
+}
